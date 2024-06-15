b41876281270a51212bf44412be91ec8e1885b6f
Vladimir Pedchenko <paralogyX@gmail.com>
2024-06-15 19:35:46 +0000

    tiny refactoring

--- testAll.py
+++ testAll.py
@@ -13,18 +13,25 @@ import combperm
 
 tst = 1
 
-if tst in (1,3): digitList = list(range(10))
+if tst in (1,3): 
+    digitList = list(range(10))
 stringList = 'abcdefhg'
 
-if tst in (1,3): digitCode = combperm.combinaionsAndPermutations(digitList)
-if tst in (2,3): stringCode = combperm.combinaionsAndPermutations(stringList)
+if tst in (1,3): 
+    digitCode = combperm.combinaionsAndPermutations(digitList)
+if tst in (2,3): 
+    stringCode = combperm.combinaionsAndPermutations(stringList)
 
 for i in range(1,5):
     print(i)
-    if tst in (1,3): digitCode.SetTypeOfCombination(i)
-    if tst in (2,3): stringCode.SetTypeOfCombination(i)
-    if tst in (1,3): print("Digital code type:", digitCode.GetTypeOfCombination())
-    if tst in (2,3): print("String code type:", stringCode.GetTypeOfCombination())
+    if tst in (1,3): 
+        digitCode.SetTypeOfCombination(i)
+    if tst in (2,3): 
+        stringCode.SetTypeOfCombination(i)
+    if tst in (1,3): 
+        print("Digital code type:", digitCode.GetTypeOfCombination())
+    if tst in (2,3): 
+        print("String code type:", stringCode.GetTypeOfCombination())
     if tst in (1,3):
         for j in range (1, digitCode.GetAmountOfElements() + 1):
             digitCode.SetItemsInCombination(j)
